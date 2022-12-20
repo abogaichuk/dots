a9a78792974afe21da0ea515a0ef904413e35703
Andrii Bohaichuk <bogaychuk.a@gmail.com>
2022-12-20 12:32:07 +0200

    added arch themes

--- install_arch.sh
+++ install_arch.sh
@@ -12,15 +12,18 @@ function aur_install {
   rm -rf $1
 }
 
-pacman -Sy --noconfirm --needed base-devel vim git tig wget curl fish fzf tmux ranger htop
+pacman -Sy --noconfirm --needed base-devel vim git tig wget curl fish fzf tmux htop
 
 pacman -S --noconfirm yajl
 aur_install package-query  # yajl required
 aur_install yaourt  # package-query required
 
 yaourt -S --noconfirm xorg xorg-xinit awesome mpd ncmpcpp rxvt-unicode-patched light gnome-icon-theme qbittorrent telegram-desktop wicd-patched wicd-gtk
-# cmst
+# cmst thunar
 sudo systemctl enable wicd sshd
 
 # screen locker:
 # https://tools.suckless.org/slock/
+#
+# sddm themes
+# https://github.com/Guidobelix/archlinux-themes-sddm
